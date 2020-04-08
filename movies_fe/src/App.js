03d86876a89ed1e2388a298236e1537dd0b41ad6
Jonathan Arnaldo <jonarnaldo@jonathans-air-2.lan>
2020-04-08 12:13:37 -0700

    adds searchbar

--- movies_fe/src/App.js
+++ movies_fe/src/App.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { GridList, GridListTile, GridListTileBar } from '@material-ui/core';
 import axios from 'axios';
 import './App.css';
+import SearchBar from './components/searchBar';
 
 function App() {
   const [list, setList] = useState(undefined);
+  const [detail, setDetail] = useState(undefined);
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [input, setInput] = useState('');
 
   const baseURL = process.env.REACT_APP_BACKEND_API;
-  console.log('baseURL', baseURL)
+  
   useEffect(() => {
     getPopular();
   }, []);
@@ -27,13 +30,39 @@ function App() {
       })
   }
 
+  const getDetail = (id) => {
+    axios
+    .get(`${baseURL}movies/movie/${id}`)
+    .then(function (response) {
+      setIsLoaded(true);
+      setDetail(response.data)
+    })
+    .catch(function (error) {
+      setIsLoaded(true);
+      setError(error)
+    })
+  }
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    setDetail(null)
+    event.preventDefault();
+  }
+
   const handleClick = (e, id) => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+    getDetail(id);
   }
 
   return (
     <div className="App">
+      <div className="App-header">
+        <SearchBar handleChange={handleChange} handleSubmit={handleSubmit} input={input} />
+      </div>
       {
         list &&
         <GridList cellHeight={500} cols={5} spacing={0}>
